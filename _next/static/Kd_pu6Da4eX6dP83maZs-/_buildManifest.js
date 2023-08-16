3677b4d15c91d108059a73798348ffc2e40aba40
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-08-16 23:17:23 +0200

    style: update

---
+++ _next/static/Kd_pu6Da4eX6dP83maZs-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-de7d5744d81255d1.js","static/chunks/989-9a39a78032956db2.js","static/chunks/pages/index-215a972a2f577412.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/V7EqLx9cf4JpcfPfmTA-0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-de7d5744d81255d1.js","static/chunks/989-9a39a78032956db2.js","static/chunks/pages/index-8ef9a15ae7366f4a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
